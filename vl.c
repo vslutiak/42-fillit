fabcf669354b03874d4c0ddf75b1ca11fd8dcd69
Vladyslav SLUTIAK <vslutiak@e2r7p7.unit.ua>
2019-03-18 21:27:27 +0200

    completed

--- vl.c
+++ vl.c
@@ -13,6 +13,39 @@
 #include "fillit.h"
 #include "stdio.h"
 
+int	ft_new_line(char *map, int len)
+{
+	int	i;
+	int	b;
+	int	count;
+
+	i = 4;
+	b = 0;
+	count = 0;
+	while (i < len)
+	{
+		while (++b < 3)
+			if (map[i] == '\n')
+				i += 5;
+			else
+				return (0);
+		b = 0;
+		if (!((map[len - 1] == '\n' && map[len - 2] == '\n') &&
+			(map[len - 3] == '#' || map[len - 3] == '.')))
+			return (0);
+		if (map[i] == '\n' && map[i + 1] == '\n'
+			&& map[i + 2] == '\0' && i + 2 != len)
+			return (0);
+		if (map[i] == '\n' && map[i + 1] == '\n')
+			i += 6;
+		else if (map[i] == '\n' && map[i + 1] == '\0')
+			break ;
+		else
+			return (0);
+	}
+	return (1);
+}
+
 int	ft_line(char *map)
 {
 	int col;
@@ -46,11 +79,11 @@ int	ft_valid(int len, char *map)
 	{
 		if (map[i] == '#' && b > 1 && map[i - 1] == '#')
 			++count;
-	 	if (map[i] == '#' && b < 19 && map[i + 1] == '#') 
+		if (map[i] == '#' && b < 19 && map[i + 1] == '#')
 			++count;
 		if (map[i] == '#' && b > 5 && map[i - 5] == '#')
-			 ++count;
-		if (map[i] == '#' && b < 15 && map[i + 5] == '#') 
+			++count;
+		if (map[i] == '#' && b < 15 && map[i + 5] == '#')
 			++count;
 		if (b == 20)
 		{
@@ -63,16 +96,27 @@ int	ft_valid(int len, char *map)
 		i++;
 		b++;
 	}
+	return (1);
+}
+
+int	ft_count(char *map, int len)
+{
+	int i;
+	int b;
+	int count;
+
 	i = 0;
-	b = 0;
+	b = 1;
 	count = 0;
+	if (!map[i])
+		return (0);
 	while (i < len)
 	{
 		if (map[i] == '#')
 			count++;
 		if (b == 20)
 		{
-			if (coll != 4)
+			if (count != 4)
 				return (0);
 			b = 0;
 			count = 0;
@@ -84,7 +128,7 @@ int	ft_valid(int len, char *map)
 	return (1);
 }
 
-int main(int argc, char const *argv[])
+int	main(int argc, char const *argv[])
 {
 	int		len;
 	int		fd;
@@ -95,11 +139,17 @@ int main(int argc, char const *argv[])
 	len = read(fd, buffer, MAX);
 	buffer[len] = '\0';
 	map = ft_strsub(buffer, 0, len);
-	//printf("%s\n", map);
+	//printf("%s", map);
 	if (ft_valid(len, map) != 1)
-		write(2, "EROR", 4);
+		write(2, "EROR\n", 5);
+	else if (ft_line(map) != 1)
+		write(2, "LINE\n", 5);
+	else if (ft_new_line(map, len) != 1)
+		write(2, "NEW\n", 4);
+	else if (ft_count(map, len) != 1)
+		write(2, "COUNT\n", 6);
+	else
+		write(2, "VALID", 5);
 	close(fd);
 	return (1);
 }
- 	
-
