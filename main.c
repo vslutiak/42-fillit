e2b5bb57ffef6c972a3a74037c6830b7de837c52
Vladyslav SLUTIAK <vslutiak@e2r7p7.unit.ua>
2019-03-24 18:47:48 +0200

    a

--- main.c
+++ main.c
@@ -1,23 +1,11 @@
-#include <stdio.h>
 #include "fillit.h"
 
-/*
- * зчитувач
- * валідатор & обрізати фігуру до її меж
- * алгоритм знаходження найменшої фігури вліво вгору
- * знаходимо крайній правий кут нанесеної фігури і переміщуємось далі
- * провіряємо на карті чи можна
- * видрук
-*/
-
-int     main(int ac, char **av)
+int main(int argc, char const *argv[])
 {
-	char	**map;
-	char	*valid;
+	int fd;
+	
+	fd = open(argv[1], O_RDONLY);
+	ft_vl(fd);
 
-	valid = ft_rd(av[1]);
-	map = ft_map(2);
-	map = ft_alg(valid, map);
-//	ft_print_map(map);
-	return (0);
+	return 0;
 }
